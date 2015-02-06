b100d483f30c688b0a268a06763bbba2aa89411d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-02-06 09:00:22 +0100

    link to check mpay status

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ end
 
 MercatorMpay24::Engine.routes.draw do
   get 'confirmation' => 'confirmations#create', :as => 'create_confirmation'
-  get 'admin/payments/:id/check_confirmation' => 'admin/payments#check_confirmation'
+  get 'admin/payments/:id/check_confirmation' => 'admin/payments#check_confirmation', :as => 'check_confirmation'
 end
