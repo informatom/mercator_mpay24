ddc79b0be602ed3bfb74aecf7717b012a279f5a5
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-06-20 08:53:00 +0200

    moved confirmations controller out of mpay namespace

---
+++ app/controllers/admin/payments_controller.rb
@@ -0,0 +1,9 @@
+class Admin::PaymentsController < ::Admin::AdminSiteController
+
+  def check_confirmation
+    @payment =  MercatorMpay24::Payment.find(params[:id])
+    @payment.check_transaction_status
+
+    redirect_to ("/admin/orders/" + @payment.order.id.to_s)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Mercator::Application.routes.draw do
   get 'admin/test_payment' => 'orders#test_payment', :as => 'test_payment'
+
+  namespace "admin" do
+    get 'payments/:id/check_confirmation' => 'payments#check_confirmation', :as => 'check_confirmation'
+  end
 end
 
 MercatorMpay24::Engine.routes.draw do
   get 'confirmation' => 'confirmations#create', :as => 'create_confirmation'
-  get 'admin/payments/:id/check_confirmation' => 'admin/payments#check_confirmation', :as => 'check_confirmation'
 end
